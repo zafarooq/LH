dd20025a6205c0c860c586ccd1c1b5ea200405e7
Zahid Farooq <zahid_farooq@hotmail.com>
2017-02-15 11:34:33 -0800

    some testing

--- ListerHaigh.Models/Validators/Department/DepartmentValidator.cs
+++ ListerHaigh.Models/Validators/Department/DepartmentValidator.cs
@@ -12,7 +12,7 @@ namespace ListerHaigh.Validators
         public DepartmentValidator()
         {
             RuleFor(x => x.Code)
-                .NotEmpty().WithMessage("Code field cannot be left blank.")
+                .NotEmpty().WithMessage("Code cannot be left blank.")
                 .Length(1, 50).WithMessage("Code cannot be greater than 50.");
             RuleFor(x => x.Name)
                 .NotEmpty().WithMessage("Name field is required")
